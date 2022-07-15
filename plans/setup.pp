8ad73be77cc9608f61febf4634278d7d98f5435d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2022-07-15 10:30:45 +0100

    Fix puppet-lint errors
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- plans/setup.pp
+++ plans/setup.pp
@@ -1,10 +1,10 @@
 plan ldap_server_setup::setup (
-  TargetSpec $targets) {
+  TargetSpec $targets
+) {
+  run_plan('ldap_server_setup::createcerts', $targets)
+  run_plan('ldap_server_setup::install_docker', $targets)
+  run_plan('ldap_server_setup::populate_ldap_entries', $targets)
+  $response = run_task('ldap_server_setup::test', $targets)
 
- run_plan('ldap_server_setup::createcerts', $targets) 
- run_plan('ldap_server_setup::install_docker', $targets) 
- run_plan('ldap_server_setup::populate_ldap_entries', $targets) 
- $response = run_task('ldap_server_setup::test', $targets)
-
- return $response
+  return $response
 }
